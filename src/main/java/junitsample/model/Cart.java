2a213e020a339832e1838b1c46afd29f950ba88f
uasano <asano.1105@gmail.com>
2015-06-24 23:43:40 +0900

    Cartインスタンス生成直後にhasNoItemsを呼び出すとNullPointerExceptionが発生していたので、デフォルトコンストラクタの中で商品リストを初期化した。

--- src/main/java/junitsample/model/Cart.java
+++ src/main/java/junitsample/model/Cart.java
@@ -1,10 +1,15 @@
 package junitsample.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cart {
 	public int userId;
 	public List<CartItem> items;
+
+	public Cart() {
+		items = new ArrayList<>();
+	}
 
 	public boolean hasNoItems() {
 		return items.isEmpty();
