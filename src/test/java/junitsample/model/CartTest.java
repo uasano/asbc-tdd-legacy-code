1fd66079c0371ba38c245c8a418efd206d7503dd
uasano <asano.1105@gmail.com>
2015-06-24 23:52:24 +0900

    カート内に商品が存在する時にhasNoItemsがfalseを返すテストを追加

--- src/test/java/junitsample/model/CartTest.java
+++ src/test/java/junitsample/model/CartTest.java
@@ -3,6 +3,9 @@ package junitsample.model;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -18,5 +21,18 @@ public class CartTest {
     @Test
     public void testカート内の商品が０件の時にhasNoItemsがtrueを返すべき() throws Exception {
         assertThat(sut.hasNoItems(), is(true));
+    }
+
+    @Test
+    public void testカート内の商品が１件の時にhasNoItemsがfalseを返すべき() throws Exception {
+        CartItem item = new CartItem();
+
+        List<CartItem> items = new ArrayList<>();
+        items.add(item);
+
+        sut.items = items;
+
+        assertThat(sut.hasNoItems(), is(false));
+
     }
 }
