d70a487b602f5006e918437e540cf2f1d93f7717
uasano <asano.1105@gmail.com>
2015-06-24 23:06:18 +0900

    カート内の商品が0件の時に例外を発生させるために、商品件数チェックを追加

--- src/main/java/junitsample/service/CartService.java
+++ src/main/java/junitsample/service/CartService.java
@@ -1,17 +1,17 @@
 package junitsample.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import junitsample.dao.CartDao;
 import junitsample.dao.OrderDao;
+import junitsample.exception.ApplicationException;
 import junitsample.model.Cart;
 import junitsample.model.CartItem;
 import junitsample.model.Order;
 import junitsample.model.OrderItem;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class CartService {
@@ -23,6 +23,10 @@ public class CartService {
 		Cart cart = cartDao.find(userId);
 		List<CartItem> list = cartDao.findAllItems(userId);
 		cart.items = list;
+
+		if (cart.items.isEmpty()) {
+			throw new ApplicationException("Cart is empty.");
+		}
 
 		Order order = new Order();
 		order.userId = userId;
